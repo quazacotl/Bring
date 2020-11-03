6fa3c6f7dcee131f622a3702f03f18b69e491f48
Vagrant <quazacotl@gmail.com>
2020-11-03 15:14:48 +0300

    1st commit

---
+++ src/js/modules/slider.js
@@ -0,0 +1,62 @@
+export default class Slider {
+    constructor(page, btns) {
+        this.page = document.querySelector(page);
+        this.slides = this.page.children;
+        this.btns = document.querySelectorAll(btns);
+        this.slideIndex = 1;
+    }
+
+    showSlides(n) {
+        if (n > this.slides.length) {
+            this.slideIndex = 1;
+        }
+
+        if (n < 1) {
+            this.slideIndex = this.slides.length;
+        }
+
+        try {
+            this.teacherPopUp.style.display = 'none';
+
+            if (this.slideIndex === 3) {
+                setTimeout(() => {
+                    this.teacherPopUp.style.display = 'block';
+                    this.teacherPopUp.classList.add('animated', 'fadeInUp');
+                }, 3000);
+            } else {
+                this.teacherPopUp.classList.remove( 'fadeInUp');
+            }
+
+        } catch (e) {}
+
+        this.slides.forEach(slide => {
+            slide.style.display = 'none';
+        });
+
+        this.slides[this.slideIndex - 1].style.display = 'block';
+    }
+
+    plusSlides(n) {
+        this.showSlides(this.slideIndex += n);
+    }
+
+    render() {
+        try {
+            this.teacherPopUp = document.querySelector('.hanson');
+        } catch (e) {}
+
+        this.btns.forEach(item => {
+            item.addEventListener('click', () => {
+                this.plusSlides(1);
+            });
+
+            item.parentNode.previousElementSibling.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.slideIndex = 1;
+                this.showSlides(this.slideIndex);
+            });
+        });
+
+        this.showSlides(this.slideIndex);
+    }
+};
